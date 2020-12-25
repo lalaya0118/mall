8769ab2076a5f1c24cb405b5d127c1ce2dcdba53
lalaya0118 <1986722673@qq.com>
2020-12-25 10:35:41 +0800

    first Day

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="recommend">
+  <div class="recommend-item" v-for="item in recommends">
+    <a :href='item.link'>
+      <img :src="item.image">
+      <span>{{item.title}}</span>
+    </a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  components: {}
+};
+</script>
+
+<style  scoped>
+  .recommend{
+    display: flex;
+    margin-top: 10px;
+    font-size: 14px;
+    padding-bottom: 30px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item{
+    flex: 1;
+    text-align: center;
+  }
+
+  .recommend img{
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
